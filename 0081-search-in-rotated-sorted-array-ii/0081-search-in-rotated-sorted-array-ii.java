4ce40ec7ef59021fcbd0e17046fa073ced168a1c
Gopal Meena <meenagopal24@gmail.com>
2023-07-16 23:14:54 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.06%) - CodeSyncProV2

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            if (nums[i] == target || nums[j] == target) return true;
+            i++;
+            j--;
+        }
+        return false;
+    }
+}

8c3827c1a095f41a4748c2f4a6f5b26d78116248
Gopal Meena <meenagopal24@gmail.com>
2023-07-16 17:54:23 +0530

    Time: 1 ms (24.25%), Space: 44.8 MB (8.63%) - CodeSyncProV2

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = Integer.MIN_VALUE;
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        convertBST(root.right);
+        if (sum==Integer.MIN_VALUE){
+            sum = root.val;
+        }else {
+            sum = sum + root.val;
+            root.val = sum;
+        }
+        // System.out.print(root.val + " ");
+        convertBST(root.left);
+
+        return root;   
+    }
+}

11c4978c670a3151bcae44dd440dc100033ba96a
Gopal Meena <meenagopal24@gmail.com>
2023-07-29 05:47:05 +0530

    Time: 4 ms (95.94%), Space: 42.5 MB (67.54%) - CodeSyncProV2

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,13 +1,17 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (sb.length() == 0) {
-                sb.append(s.charAt(i));
-            } else if (s.charAt(i) == 'B' && sb.charAt(sb.length() - 1) == 'A' || s.charAt(i) == 'D' && sb.charAt(sb.length() - 1) == 'C') {
-                sb.deleteCharAt(sb.length() - 1);
-            } else sb.append(s.charAt(i));
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        st.push(s.charAt(0));
+        for(int i=1;i<n;i++)
+        {
+            if(!st.isEmpty()&&((s.charAt(i)=='B'&&st.peek()=='A')||(s.charAt(i)=='D'&&st.peek()=='C')))
+            {
+                st.pop();
+            }
+            else
+                st.push(s.charAt(i));
         }
-        return sb.length();       
+        return st.size();
     }
 }

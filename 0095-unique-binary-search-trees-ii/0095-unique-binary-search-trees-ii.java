a661946805ae79ac7ed98ffa58f4d60e5a095b6a
Gopal Meena <meenagopal24@gmail.com>
2023-08-06 04:04:37 +0530

    Time: 1 ms (99.88%), Space: 43.5 MB (78.63%) - CodeSyncProV2

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -1,50 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeN
- *     TreeNode(int val) { this.val = val; }
- *     TreeNxode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- *
-/class Solution {
+class Solution {
     public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return helper(1, n);
+    }
+    
+    private List<TreeNode> helper(int lo, int hi) {
+        List<TreeNode> result = new ArrayList<>();
+        //base case
+        if (lo > hi) {
+            result.add(null);
+            return result;
+        }
         
-    }*/
-
-
-    class Solution {
-            public List<TreeNode> generateTrees(int n) {
-                    return n > 0 ? generate_trees(1, n) : new ArrayList<>();
-                        }
-
-                            private List<TreeNode> generate_trees(int start, int end) {
-                                    List<TreeNode> all_trees = new ArrayList<>();
-                                            if (start > end) {
-                                                        all_trees.add(null);
-                                                                    return all_trees;
-                                                                            }
-
-                                                                                    for (int i = start; i <= end; i++) {
-                                                                                                List<TreeNode> left_trees = generate_trees(start, i - 1);
-                                                                                                            List<TreeNode> right_trees = generate_trees(i + 1, end);
-
-                                                                                                                        for (TreeNode l : left_trees) {
-                                                                                                                                        for (TreeNode r : right_trees) {
-                                                                                                                                                            TreeNode current_tree = new TreeNode(i);
-                                                                                                                                                                                current_tree.left = l;
-                                                                                                                                                                                                    current_tree.right = r;
-                                                                                                                                                                                                                        all_trees.add(current_tree);
-                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                    return all_trees;
-                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                        }
-   
+        //subproblem to reach boootm
+        for (int i = lo; i <= hi; i++) {
+            List<TreeNode> left = helper(lo, i - 1);
+            List<TreeNode> right = helper(i + 1, hi);
+            //reconstruct tree from bottom to up
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    result.add(root);
+                }
+            }
+        }
+        //return list of root to last layer
+        return result;
+    }
+}

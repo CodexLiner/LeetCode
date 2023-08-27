0a4c511b0e3982af82dda5e3c468dbed23290793
Gopal Meena <meenagopal24@gmail.com>
2023-08-28 00:50:23 +0530

    Time: 3 ms (22.5%), Space: 45.9 MB (12.85%) - CodeSyncProV2

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,8 +1,23 @@
 class Solution {
+    // public static int[] twoSum(int[] numbers, int target) {
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         for (int j = i + 1; j < numbers.length; j++) {
+    //             if (numbers[i] + numbers[j] == target) return new int[]{i + 1, j + 1};
+    //         }
+    //     }
+    //     return new int[0];
+    // }
     public static int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) return new int[]{i + 1, j + 1};
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
             }
         }
         return new int[0];

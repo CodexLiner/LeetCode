1f7e071e7600e8d54dd4b9a1c77302fc33e9cc4c
Gopal Meena <meenagopal24@gmail.com>
2023-09-13 00:30:14 +0530

    Time: 7 ms (98.85%), Space: 44.8 MB (22.88%) - CodeSyncProV2

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,32 +1,47 @@
 class Solution {
     public static int minDeletions(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         int[] chars = new int[26];
         for (char c : s.toCharArray()) {
-            if (c >= 'a' && c <= 'z') {
-                chars[c - 'a']++;
-            }
+           chars[c - 'a']++;
         }
         int count = 0;
-        Arrays.sort(chars);
-        System.out.println(Arrays.toString(chars));
-        int i = 0;
-        int j = 1;
-        do {
-            if (chars[i] != 0 && chars[j] != 0 && chars[i] == chars[j]) {
-                chars[j]--;
-                count++;
-                Arrays.sort(chars);
-                i = 0;
-                j = 1;
-            } else {
-                i++;
-                j++;
-            }
-        } while (i != 26 && j != 26);
-
-
+        for(int i = 0; i < 26 ; i++ ){
+            if(chars[i] > 0 && set.contains(chars[i])){
+                while(chars[i] > 0 && set.contains(chars[i]) ){
+                    chars[i]--;
+                    count++;
+                }
+            } 
+            set.add(chars[i]);
+        }
         return count;
+        
+
+    
+
+
+
+
+        // int count = 0;
+        // Arrays.sort(chars);
+        // int i = 0;
+        // int j = 1;
+        // do {
+        //     if (chars[i] != 0 && chars[j] != 0 && chars[i] == chars[j]) {
+        //         chars[j]--;
+        //         count++;
+        //         Arrays.sort(chars);
+        //         i = 0;
+        //         j = 1;
+        //     } else {
+        //         i++;
+        //         j++;
+        //     }
+        // } while (j != 26);
+
+
+        // return count;
     }
 
 }

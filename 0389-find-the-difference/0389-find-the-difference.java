ec87354ec1f421b2b2973628d41b463263f4d8b3
Gopal Meena <meenagopal24@gmail.com>
2023-09-26 00:55:23 +0530

    Time: 2 ms (68.98%), Space: 40.5 MB (91.44%) - CodeSyncProV2

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,16 +1,23 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> hashMap = new HashMap<>();
+        // HashMap<Character, Integer> hashMap = new HashMap<>();
+        // for (int i = 0; i < t.length(); i++) {
+        //     hashMap.put(t.charAt(i), hashMap.getOrDefault(t.charAt(i), 0) + 1);
+        // }
+        // for (char c : s.toCharArray()) {
+        //     hashMap.put(c, hashMap.getOrDefault(c, 0) - 1);
+        //     if (hashMap.get(c) <= 0) hashMap.remove(c);
+
+        // }
+        // return hashMap.keySet().stream().toList().get(0);   
+        int count = 0;
         for (int i = 0; i < t.length(); i++) {
-            hashMap.put(t.charAt(i), hashMap.getOrDefault(t.charAt(i), 0) + 1);
+            count += t.charAt(i);
+            if (i < s.length()) {
+                count -= s.charAt(i);
+            }
         }
-        for (char c : s.toCharArray()) {
-            hashMap.put(c, hashMap.getOrDefault(c, 0) - 1);
-            if (hashMap.get(c) <= 0) hashMap.remove(c);
-
-        }
-
-        System.out.println(hashMap);
-        return hashMap.keySet().stream().toList().get(0);    
+       
+        return (char) count;
     }
 }

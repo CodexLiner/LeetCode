febdaa4f2e6fd856efd7397bfaecc0f4f74cd4a7
Gopal Meena <meenagopal24@gmail.com>
2023-09-29 01:11:38 +0530

    Time: 4 ms (10.44%), Space: 43.8 MB (73.96%) - CodeSyncProV2

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int[] sortArrayByParity(int[] nums) {
+        int oddCount = 0;
+        Arrays.sort(nums);
+        for (int i : nums) {
+            if (i % 2 != 0) oddCount++;
+        }
+        int[] odds = new int[oddCount];
+        int[] evens = new int[nums.length - oddCount];
+        int oddIndex = 0;
+        int evenIndex = 0;
+        for (int i : nums) {
+            if (i % 2 != 0) odds[oddIndex++] = i;
+            else evens[evenIndex++] = i;
+        }
+        Arrays.sort(odds);
+        int i = 0;
+        for (; i < evens.length; i++) {
+            nums[i] = evens[i];
+        }
+        for (int even : odds) {
+            nums[i++] = even;
+        }
+
+        return nums;
+    }
+
+}

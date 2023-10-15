3d908f5f33a8575ee83c6e4062298e388c6541d9
Gopal Meena <meenagopal24@gmail.com>
2023-10-16 01:20:31 +0530

    Time: 1 ms (86.06%), Space: 39.2 MB (49.91%) - CodeSyncProV2

--- 0755-reach-a-number/0755-reach-a-number.java
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -2,7 +2,8 @@ class Solution {
     public int reachNumber(int target) {
         int moves = 0;
         int total = 0;
-        target = Math.abs(target);
+        if(target < 0)target = -target;
+        target = target;
         while(total < target || (total-target) % 2 != 0){
             moves++;
             total += moves; 
